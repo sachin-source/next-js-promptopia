a2039341d0bddc89a0d5ad30685657dfafb69b19
sachin-source <sachinmgvt@gmail.com>
2024-06-24 19:47:15 +0530

    session callback completed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -16,15 +16,24 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({ session }) {},
+
+    async session({ session }) {
+
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
+
+        session.user.id = sessionUser._id.toString();
+
+        return session;
+    },
+
     async signIn({ profile }) {
         try {
             await connectToDB();
-            /**
-             * Check if a user already exists
-             * If not, create a new user
-             */
+
             const userExists = await User.findOne({ email : profile.email });
+
             if(!userExists){
                 await User.create({
                     email: profile.email,
