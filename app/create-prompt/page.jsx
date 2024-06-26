91ab354d1765d51be8072a255bf12a2ff479808b
sachin-source <sachinmgvt@gmail.com>
2024-06-26 10:59:55 +0530

    create-prompt ui complete

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -11,9 +11,31 @@ const CreatePrompt = () => {
 		prompt: '',
 		tag: ''
 	});
+	const { data: session } = useSession()
+	const router = useRouter()
+
 
 	const CreatePrompt = async (e) => {
+		e.preventDefault();
+		setsubmitting(true);
 
+		try {
+			const response = await fetch('/api/prompt/new', {
+				method: 'POST',
+				body: JSON.stringify({
+					prompt : post.prompt,
+					userId: session?.user.id,
+					tag: post.tag,
+				})
+			})
+			if(response.ok) {
+				router.push('/')
+			}
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setsubmitting(false);
+		}
 	}
   return (
     <Form
