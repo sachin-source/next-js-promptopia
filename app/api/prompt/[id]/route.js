6018f5775666f89097189669a99b739f337729dc
sachin-source <sachinmgvt@gmail.com>
2024-06-29 14:33:21 +0530

    delete prompt functionality

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -34,3 +34,12 @@ export const PATCH = async (request, { params }) => {
 }
 
 // DELETE (delete)
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectToDB();
+        await Prompt.findByIdAndDelete(params.id);
+        return new Response("Prompt deleted successfully", { status: 200 });
+    } catch (error) {
+        return new Response("Failed to delete prompt", { status: 500 })
+    }
+}
