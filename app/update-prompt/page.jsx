c6319c5103276cde9ec94d901c778246d2cea4ce
sachin-source <sachinmgvt@gmail.com>
2024-06-29 16:30:45 +0530

    re-using create prompt code to reuse

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,51 @@
+'use client';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import Form from '@components/Form';
+
+
+const updatePrompt = () => {
+	const [submitting, setsubmitting] = useState(false);
+	const [post, setPost] = useState({
+		prompt: '',
+		tag: ''
+	});
+	const { data: session } = useSession()
+	const router = useRouter()
+
+
+	const CreatePrompt = async (e) => {
+		e.preventDefault();
+		setsubmitting(true);
+
+		try {
+			const response = await fetch('/api/prompt/new', {
+				method: 'POST',
+				body: JSON.stringify({
+					prompt : post.prompt,
+					userId: session?.user.id,
+					tag: post.tag,
+				})
+			})
+			if(response.ok) {
+				router.push('/')
+			}
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setsubmitting(false);
+		}
+	}
+  return (
+    <Form
+		  type="Create"
+			post={post}
+			setPost={setPost}
+			submitting={submitting}
+			handleSubmit={CreatePrompt}
+		/>
+  )
+}
+
+export default updatePrompt
